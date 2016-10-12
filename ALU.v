2e89acdfba414c4341ecea088bdedaa76c83ed09
Jiayu Huang <jh054@bucknell.edu>
2016-10-12 16:13:46 -0400

    CPUM

--- ALU.v
+++ ALU.v
@@ -1,7 +1,6 @@
 module ALU(input [31:0] read_data_1,
 	   input [31:0]  read_data_2_or_immediate,
 	   input [2:0] 	 ALU_control,
-	   output reg	 zero,
 	   output reg [31:0] ALU_result);
 
   reg [31:0] temp;
@@ -21,10 +20,6 @@ module ALU(input [31:0] read_data_1,
 	         	end
 		 	default: $display("That's not a supported ALUop!");
      	endcase
-    	if (ALU_result == 0)
-	 		zero = 1;
-    	else
-	 		zero = 0;
     end
 endmodule
 
--- add4.v
+++ add4.v
@@ -1,4 +1,14 @@
 module add4(input [31:0] inval, output reg [31:0] outval);
-   always @(inval)
+   always @(*)
      outval = inval + 4;
+endmodule
+
+module idmultipurpose(input [31:0] inval, input [15:0] inval2, output reg [31:0] outval, output reg [31:0] signextended);
+   reg[31:0] extended;
+   always @(*)
+   		extended <= {{16{inval2[15]}}, inval2[15:0]};
+   		signextended <= extended;
+   		extended <= extended << 2;
+   		outval <= extended + inval;
+   end
 endmodule
