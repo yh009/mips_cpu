4cd2a768d9e5f5a6cafac3b5a2f0173c4ded073a
Kenny Rader <kenneth.rader.627@gmail.com>
2016-10-13 08:01:37 -0400

    Fixed syntax error in add4.v so that it compiles

--- add4.v
+++ add4.v
@@ -3,9 +3,13 @@ module add4(input [31:0] inval, output reg [31:0] outval);
      outval = inval + 4;
 endmodule
 
-module idmultipurpose(input [31:0] inval, input [15:0] inval2, output reg [31:0] outval, output reg [31:0] signextended);
+module idmultipurpose(input [31:0] inval,
+		      input [15:0] 	inval2, 
+		      output reg [31:0] outval, 
+		      output reg [31:0] signextended);
    reg[31:0] extended;
    always @(*)
+	      begin
    		extended <= {{16{inval2[15]}}, inval2[15:0]};
    		signextended <= extended;
    		extended <= extended << 2;
