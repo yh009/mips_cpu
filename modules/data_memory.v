5b4efda1f7203efe97102f6dcc6c50ccc1797b45
Jiayu Huang <jh054@bucknell.edu>
2016-10-20 09:35:09 -0400

    try to push again

--- modules/data_memory.v
+++ modules/data_memory.v
@@ -11,67 +11,59 @@ module data_memory (
     input mem_read,
     input [31:0] a0,        
     output [31:0] rd,       
-    output [31:0] char,   
+    output reg [31:0] char [31:0],   
     input SyscallW
 );
 
-
-
-parameter DATA_BEGIN = 32'h0040_0000 >> 2;
-parameter DATA_SEG = 32'h0041_0200 >> 2;
-parameter DATA_END = 32'h0041_0f00 >> 2;
-
-
-reg [31:0] RAM [DATA_END: DATA_BEGIN];      // like 255:0
+reg [31:0] RAM [32'h0041_0f00 >> 2: 32'h0040_0000 >> 2];      
 reg [31:0] Stack [32'hffff_f000 >> 2: 32'hffff_ffff >> 2];
 
-parameter MONITOR = 0;
 integer i, j, f1, f2, p;
 initial begin
-
     $monitor($time, "dm monitor:char %c char %c ",char,RAM[32'h100026]); 
     $readmemh("hello.v", RAM);
-    for (p = DATA_SEG; p < DATA_END; p++)
-        RAM[p] <= 32'b0;
-
-    
 end
 
 
-assign char = char_reg;
-reg [8:0] char_reg;
+reg [31:0] char_reg;
 reg printSig = 0;
 
-reg [1:0]sysbo;
-reg [31:0]syswa;
+reg [31:0]phywordaddress;
 
-parameter NEWLINE = 8'd10;
-always @(posedge SyscallW)  begin
-    sysbo = a0[1:0];
-    syswa = a0[31:2];
-    case(sysbo)
-        3: char_reg = {23'b0, printSig, RAM[syswa][31:24]};
-        2: char_reg = {23'b0, printSig, RAM[syswa][23:16]};
-        1: char_reg = {23'b0, printSig, RAM[syswa][15:8]};
-        0: char_reg = {23'b0, printSig, RAM[syswa][7:0]};
-    endcase
-
-    while (char_reg[7:0] != 8'b0)  begin
-
-        #5;
-        if (sysbo == 3) begin
-            syswa = syswa + 1;
-        end
-        sysbo = sysbo + 1;
-        printSig = !printSig;
-        case(sysbo)
-            3: char_reg = {23'b0, printSig, RAM[syswa][31:24]};
-            2: char_reg = {23'b0, printSig, RAM[syswa][23:16]};
-            1: char_reg = {23'b0, printSig, RAM[syswa][15:8]};
-            0: char_reg = {23'b0, printSig, RAM[syswa][7:0]};
-        endcase
+always @(negedge clk) begin
+    reg [0]
+    while(char_reg != 0) begin
+        char
     end
 end
+
+// always @(negedge clk)  begin
+//     sysbo = a0[1:0];
+//     syswa = a0[31:2];
+//     case(sysbo)
+//         3: char_reg = {23'b0, printSig, RAM[syswa][31:24]};
+//         2: char_reg = {23'b0, printSig, RAM[syswa][23:16]};
+//         1: char_reg = {23'b0, printSig, RAM[syswa][15:8]};
+//         0: char_reg = {23'b0, printSig, RAM[syswa][7:0]};
+//     endcase
+
+//     while (char_reg[7:0] != 8'b0)  begin
+
+//         #5;
+//         if (sysbo == 3) begin
+//             syswa = syswa + 1;
+//         end
+//         sysbo = sysbo + 1;
+//         printSig = !printSig;
+//         case(sysbo)
+//             3: char_reg = {23'b0, printSig, RAM[syswa][31:24]};
+//             2: char_reg = {23'b0, printSig, RAM[syswa][23:16]};
+//             1: char_reg = {23'b0, printSig, RAM[syswa][15:8]};
+//             0: char_reg = {23'b0, printSig, RAM[syswa][7:0]};
+//         endcase
+//     end
+// end
+
 
 wire [31:0] wa;
 assign wa = {2'b00, a[31:2]};
@@ -95,13 +87,13 @@ always @(negedge clk) begin// TODO why clk
 	    $display($time,"Write Success %x %x",word2Write,wa);
 end
         else 
-            if (wa <= DATA_END && wa >= DATA_BEGIN)
+            if (wa <= 32'h0041_0f00 >> 2 && wa >= 32'h0040_0000 >> 2)
                 RAM[wa] <= write_data;   
             else 
                 if (wa > 32'hffff_ffff >> 2)   begin
                     $display("Stack overflow");
                 end  else
-                    if (wa >DATA_END) begin
+                    if (wa >32'h0041_0f00 >> 2) begin
                         $display("Data segment overflow");
                     end
             
